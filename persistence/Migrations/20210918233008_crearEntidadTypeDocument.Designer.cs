29c07248a98dc4062dd32bb82f64a1457feb3cff
SantiagoAponte <60708791+SantiagoAponte@users.noreply.github.com>
2021-10-05 12:13:43 -0500

    Corriengo aun deploy

--- persistence/Migrations/20210918233008_crearEntidadTypeDocument.Designer.cs
+++ persistence/Migrations/20210918233008_crearEntidadTypeDocument.Designer.cs
@@ -5,9 +5,9 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using persistence;
+using Persistence;
 
-namespace persistence.Migrations
+namespace Persistence.Migrations
 {
     [DbContext(typeof(OntoSoftContext))]
     [Migration("20210918233008_crearEntidadTypeDocument")]
--- persistence/Migrations/OntoSoftContextModelSnapshot.cs
+++ persistence/Migrations/OntoSoftContextModelSnapshot.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using persistence;
+using Persistence;
 
-namespace persistence.Migrations
+namespace Persistence.Migrations
 {
     [DbContext(typeof(OntoSoftContext))]
     partial class OntoSoftContextModelSnapshot : ModelSnapshot
