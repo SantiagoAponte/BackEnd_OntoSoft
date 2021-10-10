f2bc16ecaf64ce70bbc3ed186b75a7340058747c
SantiagoAponte <60708791+SantiagoAponte@users.noreply.github.com>
2021-10-09 22:49:23 -0500

    Cambios en rama de appoinment, una sola fecha.

--- Aplication/AppoinmentsApp/Dtos/AppoinmentsDto.cs
+++ Aplication/AppoinmentsApp/Dtos/AppoinmentsDto.cs
@@ -7,8 +7,7 @@ namespace Aplication.AppoinmentsApp.Dtos
     public class AppoinmentsDto
     {
         public Guid Id {get;set;}
-        public DateTime dateInit {get;set;}
-        public DateTime dateFinal {get;set;}
+        public DateTime date {get;set;}
         public string Title {get;set;}
         public string Text {get;set;}
         public ICollection<userDto> Users {get;set;}
