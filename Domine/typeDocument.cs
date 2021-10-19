0e176581a4615a33f9d000e9eed9857a7f8780b3
SantiagoAponte <60708791+SantiagoAponte@users.noreply.github.com>
2021-10-19 09:45:01 -0500

    cambio backend en tipo documento.

--- Domine/typeDocument.cs
+++ Domine/typeDocument.cs
@@ -7,6 +7,6 @@ namespace Domine
     {
         public string Id {get;set;}
         public string description {get;set;}
-        public User userLista {get;set;}
+        public ICollection<User> userLista {get;set;}
     }
 }
