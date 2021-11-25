3724d83b50b7cf8c73ed1c1ed7b78a4add635702
SantiagoAponte <60708791+SantiagoAponte@users.noreply.github.com>
2021-11-24 20:19:18 -0500

    se extrae id de endpoint donde se traen a todos los usuarios.

--- Aplication/Security/Users/Dtos/UserPrueba.cs
+++ Aplication/Security/Users/Dtos/UserPrueba.cs
@@ -5,6 +5,7 @@ namespace Aplication.Security.Users.Dtos
 {
     public class UserPrueba
     {
+        public string Id {get;set;}
         public string Email {get;set;}
         public string fullName {get;set;}
         public string Username {get;set;}
